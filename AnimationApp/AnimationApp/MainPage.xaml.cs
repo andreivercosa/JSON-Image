9e78e71c16ab88adf75c43abc751293b787656f2
andreivercosa <andreivercosa@gmail.com>
2019-04-03 19:04:40 -0400

    v1

---
+++ AnimationApp/AnimationApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Drawing;
+
+
+namespace AnimationApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
